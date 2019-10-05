351dcfe2910b70eddd46f699e6f49a2f182002c1
Alberto Cortes <alcortes01@gmail.com>
2019-10-05 14:08:33 -0400

    Use varialbe postgresql in testinfra tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,10 +10,12 @@ def test_postgresql_is_installed(host):
     postgresql = host.package("postgresql-server")
     assert postgresql.is_installed
 
+
 def test_postgresql_running_and_enabled(host):
     postgresql = host.service("postgresql")
-    assert apache.is_running
-    assert apache.is_enabled
+    assert postgresql.is_running
+    assert postgresql.is_enabled
+
 
 def test_port_5432_is_listening(host):
     socket = host.socket("tcp://5432")
