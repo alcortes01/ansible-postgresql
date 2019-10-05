6dc6be1605f90a86c5e57dfd336c8402960e27f3
Alberto Cortes <alcortes01@gmail.com>
2019-10-05 14:04:46 -0400

    Updated tests for testinfra

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,15 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_postgresql_is_installed(host):
+    postgresql = host.package("postgresql-server")
+    assert postgresql.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_postgresql_running_and_enabled(host):
+    postgresql = host.service("postgresql")
+    assert apache.is_running
+    assert apache.is_enabled
+
+def test_port_5432_is_listening(host):
+    socket = host.socket("tcp://5432")
+    assert(socket.is_listening)
