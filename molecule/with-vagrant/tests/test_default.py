031aad2ba333ec3fcfafcf1ce0d304811fe30ee1
Alberto Cortes <alcortes01@gmail.com>
2019-11-16 21:14:48 -0500

    Added molecule scenarios with docker and vagrant

---
+++ molecule/with-vagrant/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_postgresql_is_installed(host):
+    postgresql = host.package("postgresql-server")
+    assert postgresql.is_installed
+
+
+def test_postgresql_running_and_enabled(host):
+    postgresql = host.service("postgresql")
+    assert postgresql.is_running
+    assert postgresql.is_enabled
+
+
+def test_port_5432_is_listening(host):
+    socket = host.socket("tcp://5432")
+    assert(socket.is_listening)
